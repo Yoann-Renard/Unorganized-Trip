e67cc94523955810ab15885de6b5345b66825be6
elise.rey <elise.rey@isen.yncrea.fr>
2022-03-01 13:57:03 +0100

    création des activités

---
+++ app/src/main/java/fr/isen/unorganizedtripapp/SwapChoiceActivity.kt
@@ -0,0 +1,11 @@
+package fr.isen.unorganizedtripapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SwapChoiceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_swap_choice)
+    }
+}
---
+++ app/src/main/java/fr/isen/unorganizedtripapp/TripDetailsActivity.kt
@@ -0,0 +1,11 @@
+package fr.isen.unorganizedtripapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TripDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trip_details)
+    }
+}
