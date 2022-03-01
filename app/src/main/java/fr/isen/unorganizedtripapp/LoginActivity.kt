853a6aa003076a09e594f3b21493e0687923ad5a
elise.rey <elise.rey@isen.yncrea.fr>
2022-03-01 16:13:25 +0100

    main with buttons

--- app/src/main/java/fr/isen/unorganizedtripapp/LoginActivity.kt
+++ app/src/main/java/fr/isen/unorganizedtripapp/LoginActivity.kt
@@ -1,11 +1,42 @@
 package fr.isen.unorganizedtripapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import fr.isen.unorganizedtripapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
+    lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener {
+            login()
+        }
+
+        auth = Firebase.auth
+    }
+
+    private fun login() {
+        val email = binding.loginEmail.text.toString()
+        val password = binding.loginPassword.text.toString()
+
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Erreur de connexion", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- app/src/main/java/fr/isen/unorganizedtripapp/MainActivity.kt
+++ app/src/main/java/fr/isen/unorganizedtripapp/MainActivity.kt
@@ -1,11 +1,43 @@
 package fr.isen.unorganizedtripapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.isen.unorganizedtripapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.homeActButton.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
+
+        binding.loginActButton.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        binding.registerActButton.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+        binding.navActButton.setOnClickListener {
+            startActivity(Intent(this, NavigationActivity::class.java))
+        }
+
+        binding.tripDetailsActButton.setOnClickListener {
+            startActivity(Intent(this, TripDetailsActivity::class.java))
+        }
+
+        binding.locDetailsActButton.setOnClickListener {
+            startActivity(Intent(this, LocationDetailsActivity::class.java))
+        }
+
+        binding.swapChoiceActButton.setOnClickListener {
+            startActivity(Intent(this, SwapChoiceActivity::class.java))
+        }
     }
 }
