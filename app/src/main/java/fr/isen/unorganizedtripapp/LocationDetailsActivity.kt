0c235219aba5e5f24229637b3579ea700dbbceb5
elise.rey <elise.rey@isen.yncrea.fr>
2022-03-07 15:39:18 +0100

    db & logo

--- app/src/main/java/fr/isen/unorganizedtripapp/LocationDetailsActivity.kt
+++ app/src/main/java/fr/isen/unorganizedtripapp/LocationDetailsActivity.kt
@@ -2,10 +2,13 @@ package fr.isen.unorganizedtripapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.isen.unorganizedtripapp.databinding.ActivityLocationDetailsBinding
 
 class LocationDetailsActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLocationDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_information)
+        binding = ActivityLocationDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
